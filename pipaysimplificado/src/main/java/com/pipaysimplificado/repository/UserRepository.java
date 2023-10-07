e870e22fdcb0cde41c4a51e10146be93880b19c5
Gentil Silva <silvagentil.sg@gmail.com>
2023-10-07 19:54:16 -0300

    feat: create UserRepository

---
+++ pipaysimplificado/src/main/java/com/pipaysimplificado/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.pipaysimplificado.repository;
+
+import com.pipaysimplificado.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findUserByDocument(String document);
+}
