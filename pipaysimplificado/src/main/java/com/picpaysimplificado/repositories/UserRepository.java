5c0efb6867bfdbb192978d59b65730a32ab9c27e
Gentil Silva <silvagentil.sg@gmail.com>
2023-10-07 23:06:10 -0300

    rafactor: change imports

--- pipaysimplificado/src/main/java/com/picpaysimplificado/repositories/UserRepository.java
+++ pipaysimplificado/src/main/java/com/picpaysimplificado/repositories/UserRepository.java
@@ -1,6 +1,6 @@
-package com.pipaysimplificado.repository;
+package com.picpaysimplificado.repositories;
 
-import com.pipaysimplificado.domain.user.User;
+import com.picpaysimplificado.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
